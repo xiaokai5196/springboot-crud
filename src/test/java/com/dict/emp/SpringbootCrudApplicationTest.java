fc5676218dc33d3292494610a77a2faa24168c17
zhangxiaokai <18513164006@139.com>
2020-12-02 16:57:51 +0800

    delete test

--- src/test/java/com/dict/emp/SpringbootCrudApplicationTest.java
+++ src/test/java/com/dict/emp/SpringbootCrudApplicationTest.java
@@ -1,25 +1,15 @@
 package com.dict.emp;
 
-import com.dict.emp.entity.DeptEmps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootCrudApplicationTest {
-
-    @Autowired
-    private DeptEmps deptEmps;
-
-    @Test
-    public void dpet(){
-        System.out.println(deptEmps);
-    }
 
     @Test
     public void contextLoads() {
